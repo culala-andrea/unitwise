a0688e2fbe981f3c9580582bd74d3b66da82e7ed
timbol-christian-florence <timbolchristianflorence@gmail.com>
2025-03-20 22:35:36 +0800

    speed contents

--- lib/speed.dart
+++ lib/speed.dart
@@ -22,6 +22,235 @@ class _SpeedAppState extends State<SpeedApp> {
           style: TextStyle(color: CupertinoColors.systemBlue),
         ),
       ),
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 35),
+
+                Text(
+                  'Scroll through the options below to select the conversion type',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 15),
+
+                CupertinoPicker(
+                  itemExtent: 50.0,
+                  onSelectedItemChanged: (int index) {
+                    setState(() {
+                      switch (index) {
+                        case 0:
+                          convertname = "Kilometers per Hour";
+                          converttype = "mphtokph";
+                          unitype = "Miles per Hour";
+                          _quantity.text = "";
+                          result = "0.00";
+                          break;
+                        case 1:
+                          convertname = "Miles per Hour";
+                          converttype = "kphtomph";
+                          unitype = "Kilometers per Hour";
+                          _quantity.text = "";
+                          result = "0.00";
+                          break;
+                        case 2:
+                          convertname = "Meters per Second";
+                          converttype = "fpsmps";
+                          unitype = "Feet per Second";
+                          _quantity.text = "";
+                          result = "0.00";
+                          break;
+                        case 3:
+                          convertname = "Feet per Second";
+                          converttype = "mpsfps";
+                          unitype = "Meters per Second";
+                          _quantity.text = "";
+                          result = "0.00";
+                          break;
+                      }
+                    });
+                  },
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'MPH to KPH',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'KPH to MPH',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'FPS to MPS',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'MPS to FPS',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50),
+
+                Row(
+                  children: [
+                    Text(
+                      "$unitype",
+                      style: TextStyle(
+                        color: CupertinoColors.systemBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                CupertinoTextField(
+                  placeholder: '$unitype',
+                  padding: EdgeInsets.all(10),
+                  controller: _quantity,
+                  placeholderStyle: TextStyle(
+                    fontSize: 16,
+                    color: CupertinoColors.inactiveGray,
+                  ),
+                  style: TextStyle(fontSize: 16),
+                ),
+
+                SizedBox(height: 20),
+
+                Row(
+                  children: [
+                    Text(
+                      "Result",
+                      style: TextStyle(
+                        color: CupertinoColors.systemBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: CupertinoColors.darkBackgroundGray,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('$result', style: TextStyle(fontSize: 16)),
+                        SizedBox(height: 10),
+                        Text(
+                          '$convertname',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                SizedBox(height: 15),
+
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFF446CCF),
+                  ),
+                  child: CupertinoButton(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      'Convert',
+                      style: TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        double num = double.parse(_quantity.text);
+
+                        if (converttype == "mphtokph") {
+                          double res = num * 1.60934;
+                          res = double.parse(res.toStringAsFixed(2));
+                          result = res.toString();
+                        } else if (converttype == "kphtomph") {
+                          double res = num / 1.60934;
+                          res = double.parse(res.toStringAsFixed(2));
+                          result = res.toString();
+                        } else if (converttype == "fpsmps") {
+                          double res = num * 0.3048;
+                          res = double.parse(res.toStringAsFixed(3));
+                          result = res.toString();
+                        } else if (converttype == "mpsfps") {
+                          double res = num / 0.3048;
+                          res = double.parse(res.toStringAsFixed(3));
+                          result = res.toString();
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
