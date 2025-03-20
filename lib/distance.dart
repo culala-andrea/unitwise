419c976581864741a08fa3fb878d745cee2e3d62
artienda-mary-joyce-martin <joyce.art25@gmail.com>
2025-03-20 22:32:29 +0800

    distance contents

--- lib/distance.dart
+++ lib/distance.dart
@@ -23,6 +23,228 @@ class _DistanceAppState extends State<DistanceApp> {
           style: TextStyle(color: CupertinoColors.systemBlue),
         ),
       ),
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 35),
+
+                Text(
+                  'Scroll through the options below to select the conversion type',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 15),
+
+                CupertinoPicker(
+                  itemExtent: 50.0,
+                  onSelectedItemChanged: (int index) {
+                    setState(() {
+                      switch (index) {
+                        case 0:
+                          convertionvalue = 0.621371;
+                          convertname = "Miles";
+                          if (unitype != "Kilometer") {
+                            unitype = "Kilometer";
+                            _quantity.text = "";
+                            result = "0.00";
+                          }
+                          break;
+                        case 1:
+                          convertionvalue = 1.60934;
+                          convertname = "Kilometers";
+                          if (unitype != "Miles") {
+                            unitype = "Miles";
+                            _quantity.text = "";
+                            result = "0.00";
+                          }
+                          break;
+                        case 2:
+                          convertionvalue = 3.28084;
+                          convertname = "Feet";
+                          if (unitype != "Meters") {
+                            unitype = "Meters";
+                            _quantity.text = "";
+                            result = "0.00";
+                          }
+                          break;
+                        case 3:
+                          convertionvalue = 0.3048;
+                          convertname = "Meters";
+                          if (unitype != "Feet") {
+                            unitype = "Feet";
+                            _quantity.text = "";
+                            result = "0.00";
+                          }
+                          break;
+                      }
+                    });
+                  },
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'Kilometers to Miles',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'Miles to Kilometers',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'Meters to Feet',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          'Feet to Meters',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50),
+
+                Row(
+                  children: [
+                    Text(
+                      "$unitype",
+                      style: TextStyle(
+                        color: CupertinoColors.systemBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                CupertinoTextField(
+                  placeholder: '$unitype',
+                  padding: EdgeInsets.all(10),
+                  controller: _quantity,
+                  placeholderStyle: TextStyle(
+                    fontSize: 16,
+                    color: CupertinoColors.inactiveGray,
+                  ),
+                  style: TextStyle(fontSize: 16),
+                ),
+
+                SizedBox(height: 20),
+
+                Row(
+                  children: [
+                    Text(
+                      "Result",
+                      style: TextStyle(
+                        color: CupertinoColors.systemBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: CupertinoColors.darkBackgroundGray,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('$result', style: TextStyle(fontSize: 16)),
+                        SizedBox(height: 10),
+                        Text(
+                          '$convertname',
+                          style: TextStyle(
+                            color: CupertinoColors.systemBlue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                SizedBox(height: 15),
+
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFF446CCF),
+                  ),
+                  child: CupertinoButton(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      'Convert',
+                      style: TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        double num = double.parse(_quantity.text);
+
+                        double res = num * convertionvalue;
+                        result = res.toString();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
