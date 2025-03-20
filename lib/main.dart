32c2cc0e0594003348017b977d8ca18fa037ed23
culala-andrea <andreaculala78@gmail.com>
2025-03-20 22:09:03 +0800

    developer profile

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,106 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold();
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Row(
+          children: [
+            Image.asset('images/logo.png', height: 20, width: 20),
+            SizedBox(width: 10),
+            Text('Uniwise', style: TextStyle(fontSize: 15)),
+          ],
+        ),
+
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Icon(CupertinoIcons.profile_circled),
+          onPressed: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: Text('Team Members'),
+                  content: Column(
+                    children: [
+                      SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ClipOval(
+                            child: Image.asset(
+                              'images/joy.jpg',
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          ClipOval(
+                            child: Image.asset(
+                              'images/vianney.jpg',
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          ClipOval(
+                            child: Image.asset(
+                              'images/andrea.jpg',
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ClipOval(
+                            child: Image.asset(
+                              'images/dexter.jpg',
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          ClipOval(
+                            child: Image.asset(
+                              'images/christian.jpg',
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      Divider(),
+                      SizedBox(height: 5),
+                      Text('Artienda, Mary Joyce'),
+                      Text('Bulanadi, JhonVianey '),
+                      Text('Culala, Andrea'),
+                      Text('Gomex, Dexter '),
+                      Text('Timbol, Christian Florence'),
+                    ],
+                  ),
+
+                  actions: [
+                    CupertinoButton(
+                      child: Text(
+                        'Close',
+                        style: TextStyle(color: CupertinoColors.destructiveRed),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
